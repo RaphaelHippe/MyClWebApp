c2ccd4b146808f0d17b9831516b0199d327976dc
youchra <nemmo@gmx.de>
2015-04-02 23:49:24 +0200

    commit to push for work on laptop

--- scripts/controllers/join.js
+++ scripts/controllers/join.js
@@ -11,6 +11,8 @@ angular.module('webappApp')
   .controller('JoinCtrl', function ($scope, close) {
     $scope.modalTitle = 'Join CompeteLeague today!';
     $scope.modalSubmit = 'Join';
+    $scope.test = 'eric';
+    $scope.template = 'views/join.html';
     $scope.close = function(result) {
      close(result, 500); // close, but give 500ms for bootstrap to animate
     };
